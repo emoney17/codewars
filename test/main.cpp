7edba20189f4c59414362e2dfbd22bb627b26521
emoney17 <emmanuel.million@tutanota.com>
2023-04-07 08:09:32 -0500

    commit

--- test/main.cpp
+++ test/main.cpp
@@ -1,55 +1,49 @@
-//Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
-//Examples
+// Given the string representations of two integers, return the string representation of the sum of those integers.
 
-//pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
-//pigIt('Hello world !');     // elloHay orldway !
+// For example:
 
+// sumStrings('1','2') // => '3'
 
-#include <iostream>
-#include <vector>
+// A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
 
-std::string pig_it(std::string str)
-{
-	std::vector<std::string> v;
-	std::string temp;
-	int size = str.length();
-	for (int i = 0; i < size + 1; i++)
-	{
-		if (str[i] == ' ' || i == size)
-		{
-			v.push_back(temp);
-			temp.clear();
-		}
-		else
-		{
-			temp.push_back(str[i]);
-		}
-	}
-	for (auto i : v) std::cout << i << std::endl;
+// #include <iostream>
+// #include <cmath>
 
-	int sizev = v.size();
-	std::string ans;
-	for (int i = 0; i < sizev; i++)
-	{
-		if (v[i] == "!" || v[i] == "." || v[i] == "?" || v[i] == ",")
-			temp = v[i];
-		else
-		{
-			temp = v[i].substr(1, v[i].length());
-			temp.push_back(v[i][0]);
-			temp.push_back('a');
-			temp.push_back('y');
-		}
-		ans.append(temp);
-		ans.push_back(' ');
-	}
-	ans.pop_back();
-	std::cout << ans << std::endl;
-	return ans;
-}
+// std::string sum_strings(const std::string & a, const std::string & b)
+// {
+// 	unsigned long element;
+
+// 	unsigned long total = 0;
+// 	int asize = a.length();
+// 	unsigned long acounter = pow(10, asize - 1);
+// 	element = 0;
+// 	for (int i = 0; i < asize; i++)
+// 	{
+// 		element += ((int) a[i]) - 48;
+// 		element *= acounter; 
+// 		total += element;
+// 		element = 0;
+// 		acounter /= 10;
+// 	}
+
+// 	int bsize = b.length();
+// 	unsigned long bcounter = pow(10, bsize - 1);
+// 	element = 0;
+// 	for (int i = 0; i < bsize; i++)
+// 	{
+// 		element += ((int) b[i]) - 48;
+// 		element *= bcounter; 
+// 		total += element;
+// 		element = 0;
+// 		bcounter /= 10;
+// 	}
+// 	return std::to_string(total);
+// }
+
 
 int main(int argc, char * argv[])
 {
-	pig_it("O tempora o mores !");
+
+	std::cout << sum_strings("1908874359", "1908874359") << '\n';
 	return 0;
 }
